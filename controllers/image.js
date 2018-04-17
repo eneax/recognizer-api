47350cf9554efecd703799f80d83fbfa9c4d9d21
eneax <19818937+eneax@users.noreply.github.com>
2018-04-17 11:21:51 +0200

    Initial commit

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'd2fc1b6e6c9242d988332308c96fdeb7'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
---
+++ controllers/profile.js
@@ -0,0 +1,18 @@
+const handleProfileGet = (req, res, db) => {
+	const { id } = req.params;
+	db.select('*').from('users')
+		.where({
+			id: id
+		})
+		.then(user => {
+			if (user.length) {
+				res.json(user[0]);	
+			} else {
+				res.status(400).json('error getting user');
+			}
+		})
+}
+
+module.exports = {
+	handleProfileGet
+}
