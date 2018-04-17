7f3431bc2b768423299d2c6e39e0eac54e23605a
eneax <19818937+eneax@users.noreply.github.com>
2018-04-17 14:08:02 +0200

    Update API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: process.env.API_CLARIFAI;
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
