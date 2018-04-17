712fb68df0702a6e1fffd01e8e9ee3b9ae8ca0ae
eneax <19818937+eneax@users.noreply.github.com>
2018-04-17 12:13:01 +0200

    Fix PORT

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log(`App is running on port ${process.env.PORT} ...`);
+	console.log(`App is running on port ${process.env.PORT}`);
 });
 
 
