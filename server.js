d5240fee402cb49a6aa3e42ac74359fd08f4f802
eneax <19818937+eneax@users.noreply.github.com>
2018-04-17 12:15:20 +0200

    Change response

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = knex({
   }
 });
 
-app.get('/', (req, res) => { res.send(database.users) });
+app.get('/', (req, res) => { res.send('it is working!') });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
